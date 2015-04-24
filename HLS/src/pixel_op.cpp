24dd3ce666d49d94fec904e7504d7b96ca37520d
sukinull <sukinull@gmail.com>
2015-04-25 00:46:24 +0800

    dummy hls::Filter2D example
    // TODO: decompose hls::Filter2D into LineBuffers flow

--- HLS/src/pixel_op.cpp
+++ HLS/src/pixel_op.cpp
@@ -8,9 +8,25 @@ void pixelq_op(AXI_STREAM& src_axi, AXI_STREAM& dst_axi, int rows, int cols) {
 #pragma HLS RESOURCE core=AXI_SLAVE variable=cols metadata="-bus_bundle CONTROL_BUS"
 #pragma HLS RESOURCE core=AXI_SLAVE variable=return metadata="-bus_bundle CONTROL_BUS"
 
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_8UC3> img;
+	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_8UC3> img1, img2;
+
+	const int k_dummy[3][3] = { { 0, 0, 0 }, //
+			{ 0, 1, 0 }, //
+			{ 0, 0, 0 }, //
+			};
+
+	hls::Window<3, 3, int> kernel;
+	hls::Point_<int> anchor;
+
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			kernel.val[i][j] = k_dummy[i][j];
+
+	anchor.x = -1;
+	anchor.y = -1;
 
 #pragma HLS dataflow
-	hls::AXIvideo2Mat(src_axi, img);
-	hls::Mat2AXIvideo(img, dst_axi);
+	hls::AXIvideo2Mat(src_axi, img1);
+	hls::Filter2D(img1, img2, kernel, anchor);
+	hls::Mat2AXIvideo(img2, dst_axi);
 }
